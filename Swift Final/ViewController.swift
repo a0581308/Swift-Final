d24d028633ecff420204d0395f60f466b14c0db3
Student <student@amarillos-macbook-pro.local>
2019-02-21 08:48:31 -0600

    Initial Commit

---
+++ Swift Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Final
+//
+//  Created by Student on 2/21/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
